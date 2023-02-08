e617507b09ced26b815ac516fef6cafc3b0dc82a
Ahmed Bahaj <70455009+ahmedbahaj@users.noreply.github.com>
2023-02-08 10:38:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import RPi.GPIO as GPIO
 import time
 import smtplib
 
+# The script sets up the flame sensor on pin 21 (change this to the correct pin number for your setup) and waits for the sensor to detect a fire. 
+# When a fire is detected, the send_email function is called to send an email with the subject "Fire Alert!" and the message "A fire has been detected."
 FLAME_SENSOR_PIN = 21  # change this to the pin number your flame sensor is connected to
 
 GPIO.setmode(GPIO.BCM)
