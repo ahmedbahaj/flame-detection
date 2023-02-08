4e55b5d4009c3cdcab3c7b4bba6aa5729ce56b2b
Ahmed Bahaj <70455009+ahmedbahaj@users.noreply.github.com>
2023-02-08 10:36:36 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+import RPi.GPIO as GPIO
+import time
+import smtplib
+
+FLAME_SENSOR_PIN = 21  # change this to the pin number your flame sensor is connected to
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(FLAME_SENSOR_PIN, GPIO.IN)
+
+def send_email(subject, message):
+    smtp_server = "smtp.gmail.com"
+    smtp_port = 587
+    smtp_username = "your_email@gmail.com"
+    smtp_password = "your_email_password"
+    recipient = "recipient_email@example.com"
+
+    message = f"Subject: {subject}\n\n{message}"
+
+    try:
+        with smtplib.SMTP(smtp_server, smtp_port) as smtp:
+            smtp.ehlo()
+            smtp.starttls()
+            smtp.login(smtp_username, smtp_password)
+            smtp.sendmail(smtp_username, recipient, message)
+            print("Email sent successfully")
+    except Exception as e:
+        print(f"Error sending email: {e}")
+
+def detect_fire():
+    while True:
+        if GPIO.input(FLAME_SENSOR_PIN) == 1:
+            print("Fire detected")
+            send_email("Fire Alert!", "A fire has been detected.")
+            break
+        time.sleep(1)
+
+if __name__ == "__main__":
+    detect_fire()
